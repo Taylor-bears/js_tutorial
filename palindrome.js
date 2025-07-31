9cb4fe793990b02501a5afcbdc7d9509efbc5df8
Taylor-bears <1993834769@qq.com>
2025-07-31 10:24:04 +0800

    finish 5

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+// 反转一个字符串
+function reverse(string) {
+    return Array.from(string).reverse().join("");
+}
+
+function palindrome(string) {
+    let processedContent = string.toLowerCase();
+    return processedContent === reverse(processedContent);
+}
